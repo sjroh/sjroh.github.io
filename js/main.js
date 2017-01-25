60d30bc73a96613bf221b578c50696274ffed5ce
sjroh <sijine@gmail.com>
2017-01-24 21:36:15 -0600

    layout updated done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $(function() {
   $('.detail-show').click(function(){
-    $(this).closest('.detail-hide').css("background-color", "yellow"); //.show();
-    $(this).closest('.project-detail').css("background-color", "yellow"); //.show();
-    $(this).css("background-color", "yellow"); //.hide();
+    $(this).siblings('.detail-hide').show();
+    $(this).parent().parent().find('.project-detail').show();
+    $(this).hide();
   });
-
   $('.detail-hide').click(function(){
-    $(this).closest('.detail-show').css("background-color", "red"); //.show();
-    $(this).closest('.project-detail').css("background-color", "red");//.hide();
-    $(this).css("background-color", "red");//.hide();
+    $(this).siblings('.detail-show').show();
+    $(this).parent().parent().find('.project-detail').hide();
+    $(this).hide();
+  });
+  $('.item-skill').click(function(){
+    $( '#list-skill li' ).each(function( i ) {
+      $(this).removeClass('active');
+    });
+    $(this).addClass('active');
+
+    var selected_skill = $(this).attr('value');
+    if (selected_skill === 'all') {
+      $( '#list-experience li' ).each(function( i ) {
+        $(this).show();
+      });
+      $( '#list-project li' ).each(function( i ) {
+        $(this).show();
+      });
+    } else {
+      $( '#list-experience li' ).each(function( i ) {
+        $(this).hide();
+      });
+      $( '#list-project li' ).each(function( i ) {
+        $(this).hide();
+      });
+      $("#list-project li[skill*='" + selected_skill + "']").show();
+      $("#list-experience li[skill*='" + selected_skill + "']").show();
+    }
   });
 });
