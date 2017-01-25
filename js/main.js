ef644280409eab78463d522920d2bd429f5b38a0
sjroh <sijine@gmail.com>
2017-01-24 19:18:37 -0600

    resume json file removed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(function() {
+  $('.detail-show').click(function(){
+    $(this).closest('.detail-hide').show();
+    $(this).closest('.project-detail').show();
+    $(this).hide();
+  });
+
+  $('.detail-hide').click(function(){
+    $(this).closest('.detail-show').show();
+    $(this).closest('.project-detail').hide();
+    $(this).hide();
+  });
+});
