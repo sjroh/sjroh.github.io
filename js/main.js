743fc300ffbc3830c4e2e21f821aee961636918d
sjroh <sijine@gmail.com>
2017-01-24 19:22:41 -0600

    resume.json removed
    no need

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function() {
   $('.detail-show').click(function(){
-    $(this).closest('.detail-hide').show();
-    $(this).closest('.project-detail').show();
-    $(this).hide();
+    $(this).closest('.detail-hide').css("background-color", "yellow"); //.show();
+    $(this).closest('.project-detail').css("background-color", "yellow"); //.show();
+    $(this).css("background-color", "yellow"); //.hide();
   });
 
   $('.detail-hide').click(function(){
-    $(this).closest('.detail-show').show();
-    $(this).closest('.project-detail').hide();
-    $(this).hide();
+    $(this).closest('.detail-show').css("background-color", "red"); //.show();
+    $(this).closest('.project-detail').css("background-color", "red");//.hide();
+    $(this).css("background-color", "red");//.hide();
   });
 });
